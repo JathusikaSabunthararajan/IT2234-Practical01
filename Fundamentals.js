99da0ac7c07ca162bafeae991b05fa065501dbe9
Jathusika Sabunthararajan <jathurajan2002@gmail.com>
2025-03-13 12:21:22 +0530

    Add files via upload

---
+++ Fundamentals.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+
+let x=10;
+let y=20;
+
+let add = x+y;
+let sub = y-x;
+let mult = x*y;
+let div = y/x;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
